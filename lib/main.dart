1ac66ccab4d993729a88a823cac34d863c2ca674
LIZBETH_GARCIA_LARA <a.21308051280483@cbtis128.edu.mx>
2024-02-29 20:05:56 +0000

    Actualizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,85 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppTabBar extends StatelessWidget {
+  const AppTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo TabBar",
       theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MiPaginaInicial(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+}
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
+    return DefaultTabController(
+        length: 5,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("LibreriaLG Lizbeth Garcia"),
+            bottom: TabBar(tabs: [
+              Tab(
+                text: "l1",
+                icon: Icon(Icons.ac_unit),
+              ),
+              Tab(
+                text: "pdf",
+                icon: Icon(Icons.account_balance_outlined),
+              ),
+              Tab(
+                text: "l3",
+                icon: Icon(Icons.abc_sharp),
+              ),
+              Tab(
+                text: "l4",
+                icon: Icon(Icons.abc),
+              ),
+              Tab(
+                text: "l5",
+                icon: Icon(Icons.castle_sharp),
+              ),
+            ] //texto icono),
+                ),
+          ),
+          body: TabBarView(
+            children: const <Widget>[
+              Center(
+                child: Icon(Icons.auto_stories_outlined,
+                    size: 350, color: Colors.brown),
+              ),
+              Center(
+                child:
+                    Icon(Icons.account_balance, size: 350, color: Colors.cyan),
+              ),
+              Center(
+                child: Icon(Icons.add_card, size: 350, color: Colors.green),
+              ),
+              Center(
+                child: Icon(Icons.account_circle,
+                    size: 350, color: Colors.blueAccent),
+              ),
+              Center(
+                child: Icon(Icons.add_alert_outlined,
+                    size: 350, color: Colors.pink),
+              ),
+            ],
+          ),
+        ));
+  } //widgets
 }
